bef520b2aceedabb1df96d7eb93a8f3165a5f740
elliotseymour97 <elliotseymourkth@gmail.com>
2026-05-11 10:10:10 +0200

    Fix db and admin password

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'change-me-in-production')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///sweepstake.db')
+    _db_url = os.environ.get('DATABASE_URL', 'sqlite:///sweepstake.db')
+    SQLALCHEMY_DATABASE_URI = _db_url.replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FOOTBALL_DATA_API_KEY = os.environ.get('FOOTBALL_DATA_API_KEY', '')
     COMPETITION_CODE = os.environ.get('COMPETITION_CODE', 'WC')
