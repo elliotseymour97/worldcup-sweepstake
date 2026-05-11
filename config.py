556950defb7e52b31efb3a57c302570f327b66a9
elliotseymour97 <elliotseymourkth@gmail.com>
2026-05-11 10:18:37 +0200

    bug fix

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'change-me-in-production')
     _db_url = os.environ.get('DATABASE_URL', 'sqlite:///sweepstake.db')
-    SQLALCHEMY_DATABASE_URI = _db_url.replace('postgres://', 'postgresql://', 1)
+    SQLALCHEMY_DATABASE_URI = _db_url.replace('postgres://', 'postgresql+pg8000://', 1).replace('postgresql://', 'postgresql+pg8000://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FOOTBALL_DATA_API_KEY = os.environ.get('FOOTBALL_DATA_API_KEY', '')
     COMPETITION_CODE = os.environ.get('COMPETITION_CODE', 'WC')
